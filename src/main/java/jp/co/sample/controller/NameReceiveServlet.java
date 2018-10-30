5eaec2d18f41eced4a246a314a1c896b73a2dfe4
momosenda <momo.senda@rakus.co.jp>
2018-10-30 15:03:42 +0900

    javaマラソン１kmt

---
+++ src/main/java/jp/co/sample/controller/NameReceiveServlet.java
@@ -0,0 +1,35 @@
+package jp.co.sample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.sample.form.NameForm;
+
+/**
+ * @author momosenda
+ * 
+ * @return 名前の表示画面
+ *
+ */
+@Controller
+@RequestMapping("/")
+public class NameReceiveServlet {
+	@RequestMapping("/")
+	// 名前の情報を受け取る
+	public String index() {
+		return "inputname";
+	}
+
+	@RequestMapping("/toShow")
+
+	public String toShow(NameForm form, Model model) {
+		String name = form.getName();
+		// モデルスコープに格納
+		model.addAttribute("name", name);
+
+		// フォワード
+		return "outputname";
+	}
+
+}
---
+++ src/main/java/jp/co/sample/form/NameForm.java
@@ -0,0 +1,15 @@
+package jp.co.sample.form;
+
+public class NameForm {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
